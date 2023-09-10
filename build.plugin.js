8f5d63af277221c904294e2535b6cc97f5c19e8b
popstarfreas <6698249+popstarfreas@users.noreply.github.com>
2023-09-10 12:48:05 +0100

    build: Add missing build step

--- build.plugin.js
+++ build.plugin.js
@@ -4,3 +4,4 @@ var child_process = require("child_process");
 child_process.execSync("yarn remove terrariaserver-lite", { stdio: "inherit" })
 child_process.execSync("yarn add ../../pluginreference", { stdio: "inherit" })
 child_process.execSync("yarn build", { stdio: "inherit" })
+fs.renameSync("./build", "./plugin")
